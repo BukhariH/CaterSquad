433043e4921d21f3fda5914b5bf22b81f9c05b98
Lunding <lunding@me.com>
2015-07-22 11:14:07 +0200

    email updates.

--- catersquad.rb
+++ catersquad.rb
@@ -29,7 +29,17 @@ class Catersquad < Sinatra::Base
     participants.each do |participant|
       hash = Digest::SHA1.hexdigest(participant + SecureRandom.hex)
       parts_db.insert(:email => participant, :hash => hash, :event_id => event_id, :meal_id => params["meal_id"])
-      HTTParty.post("https://api.sendgrid.com/api/mail.send.json", query: {api_user: 'bukharih', api_key: 'ABN2a6jNCSvEGV6XFIYEX', to: participant, toname: participant, subject: 'Time to pick your meal!', html: "Hey<br>You have been invited to: #{params["title"]}<br>Your manager gave the following description of the meeting:<br>#{params["description"]}<br>It will be from: #{params["start"]} to #{params["end"]}<br>Here's a link to pick your meal: <a href='http://app.catersquad.com/participant?hash=#{hash}'>http://app.catersquad.com/participant?hash=#{hash}</a> ",  from: 'hello@catersquad.com'})
+      mail = <<-EOS
+      <h1>Hey</h1>
+      You have been invited to:<br>
+      <h2>#{params["title"]}</h2><br>
+      Description:<br>#{params["description"]}<br><br>
+      It will be from: #{params["start"]} to #{params["end"]}<br><br>
+      Here's a link to pick your meal: <a href='http://app.catersquad.com/participant?hash=#{hash}'>http://app.catersquad.com/participant?hash=#{hash}</a><br><br>
+      Best regards<b>
+      CaterSquad
+      EOS
+      HTTParty.post("https://api.sendgrid.com/api/mail.send.json", query: {api_user: 'bukharih', api_key: 'ABN2a6jNCSvEGV6XFIYEX', to: participant, toname: participant, subject: 'Time to pick your meal!', html: mail,  from: 'hello@catersquad.com'})
     end
 
     {:status => "success", :event_id => event_id}.to_json
